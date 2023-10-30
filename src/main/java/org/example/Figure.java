6d7104dd942c3b7393aa2ee0cec98f56d7e6bb86
Paul1435 <49119046+Paul1435@users.noreply.github.com>
2023-10-30 12:45:24 +0300

    Delete src/main/java/org directory

--- src/main/java/org/example/Figure.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-abstract public class Figure implements Movable {
-    abstract boolean isPointInside(Point toCheck);
-
-    @Override
-    public abstract void move(int dx, int dy);
-}
--- src/main/java/org/example/Movable.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public interface Movable {
-    void move(int dx, int dy);
-
-}
