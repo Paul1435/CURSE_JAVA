116e57ab6b0388867d25f05acdc000957c4155e1
Paul1435 <49119046+Paul1435@users.noreply.github.com>
2023-11-29 03:39:48 +0300

    Delete 16lab directory

--- 16lab/src/main/java/org/crock/winterschool/containers/Client.java
+++
@@ -1,4 +0,0 @@
-package org.crock.winterschool.containers;
-
-public record Client(int clientId, String firstName, String secondName, String numberPhone, int petId) {
-}
--- 16lab/src/main/java/org/crock/winterschool/containers/Pet.java
+++
@@ -1,4 +0,0 @@
-package org.crock.winterschool.containers;
-
-public record Pet(int petId, String name, int age) {
-}
